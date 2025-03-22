35db05efbe86a88fc08b944a7e7f3136eb914372
deoxyribonucleicacid69 <glebhleb226228@gmail.com>
2025-03-23 00:49:38 +0300

    Add files via upload

---
+++ project1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Library69;
+
+
+
+class Programm
+{
+    static void Main()
+    {
+        List<Visitor> visitors = new List<Visitor>();
+        string inputFilePath = String.Empty;
+        string outputFilePath = String.Empty;
+        Menu menu = new Menu();
+        int index = 0;
+        menu.LaunchingMainMenu(index, FieldMenu.MainMenu,ref visitors, ref inputFilePath);
+        
+    }
+}
